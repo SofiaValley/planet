b44132fb4f7b05892466a4ce3b6f238eea505b00
Alexander Todorov <atodorov@otb.bg>
2013-07-08 22:26:42 +0300

    add s3cmd

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
-setup(name='SofiaValleyPlanet',
-      version='1.0',
-      description='Planet RSS feed for SofiaValley.com',
-      author='Alexander Todorov',
-      author_email='atodorov@otb.bg',
-      url='http://planet.sofiavalley.com',
-     )
+setup(
+    name='SofiaValleyPlanet',
+    version='1.0',
+    description='Planet RSS feed for SofiaValley.com',
+    author='Alexander Todorov',
+    author_email='atodorov@otb.bg',
+    url='http://planet.sofiavalley.com',
+    install_requires=[
+        's3cmd==1.1.0-beta3',
+    ],
+)
